294fe46cf2ef0a6601d48e8302e8aefc05f464ff
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2023-01-21 17:23:08 +0100

    toucher la bonne offre

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,13 +1,36 @@
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Product = () => {
   const { id } = useParams();
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <div>
-      <p>Product id : {id}</p>
-      <Link to={"/"}>Link to Home Page</Link>
-    </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        // console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <p>Chargement de la fiche en cours...</p>
+  ) : (
+    console.log(data.offfers._id)
+    // <div>
+    //   <p>Product id : {id}</p>
+    //   <Link to={"/"}>Link to Home Page</Link>
+    // </div>
   );
 };
 
