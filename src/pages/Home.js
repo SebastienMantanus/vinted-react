9d6dcb55b806c3f04f9b1f2335a02f681c615bde
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2023-01-21 16:43:40 +0100

    api connected

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,31 @@
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <p>En cours de chargement...</p>
+  ) : (
     <div>
       <Hero />
 
