9a4c03e7abe131a4fc2edc02d7ce8521d726acc3
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2023-01-28 16:18:34 +0100

    css fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,40 +29,42 @@ const Home = () => {
     <div>
       <Hero />
       <div className="offers-grid">
-        {data.offers.map((element, index) => {
-          return (
-            <div key={element._id}>
-              {element.owner && (
-                <Link to={`/product/${element._id}`}>
-                  <div className="offer">
-                    <div className="offer-owner">
+        <div className="offers-container">
+          {data.offers.map((element, index) => {
+            return (
+              <div key={element._id}>
+                {element.owner && (
+                  <Link to={`/product/${element._id}`}>
+                    <div className="offer">
+                      <div className="offer-owner">
+                        <img
+                          src={element.owner.account.avatar.secure_url}
+                          alt="owner avatar"
+                        />
+                        <p>{element.owner.account.username}</p>
+                      </div>
+
                       <img
-                        src={element.owner.account.avatar.secure_url}
-                        alt="owner avatar"
+                        className="offer-image"
+                        src={element.product_image.secure_url}
+                        alt="product illustrtion"
                       />
-                      <p>{element.owner.account.username}</p>
-                    </div>
+                      <div className="offer-details">
+                        <p>
+                          {element.product_price.toFixed(2).replace(".", ",")} €
+                        </p>
 
-                    <img
-                      className="offer-image"
-                      src={element.product_image.secure_url}
-                      alt="product illustrtion"
-                    />
-                    <div className="offer-details">
-                      <p>
-                        {element.product_price.toFixed(2).replace(".", ",")} €
-                      </p>
-
-                      {element.product_details.map((element, index) => {
-                        return <p key={index}>{element.MARQUE}</p>;
-                      })}
+                        {element.product_details.map((element, index) => {
+                          return <p key={index}>{element.MARQUE}</p>;
+                        })}
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              )}
-            </div>
-          );
-        })}
+                  </Link>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
